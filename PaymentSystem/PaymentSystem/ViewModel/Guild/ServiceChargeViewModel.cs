1f2b45106a878fbd2149a7bee34e9fe3b082a8cf
robin <robin@shin-her.com.tw>
2024-10-16 18:21:45 +0800

    Service Change 畫面建置

--- PaymentSystem/PaymentSystem/ViewModel/Guild/ServiceChargeViewModel.cs
+++ PaymentSystem/PaymentSystem/ViewModel/Guild/ServiceChargeViewModel.cs
@@ -1,9 +1,20 @@
-﻿namespace PaymentSystem.ViewModel
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentSystem.ViewModel
 {
     public record ServiceChargeViewModel
     {
+        [Required]
+        [DisplayName("公會會員編號")]
         public string MemberId { get; set; }
 
+        [Required]
+        [DisplayName("員工編號")]
+        public string EmpId { get; set; }
+
+        [Required]
+        [DisplayName("每月扣繳服務費")]
         public string Amount { get; set; }
     }
 }
